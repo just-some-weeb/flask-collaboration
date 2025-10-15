54c83ede0c4b9bcf3d39ce38f546538edba48b47
just-some-weeb <usmanulhaq273@gmail.com>
2025-10-15 21:19:39 +0500

    Add dashboard functionality

--- housepk_app.py
+++ housepk_app.py
@@ -15,3 +15,15 @@ def health():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+
+@app.route('/dashboard')
+def dashboard():
+    print("Dashboard accessed by user")
+    return jsonify({
+        "message": "Welcome to Dashboard",
+        "data": {
+            "total_properties": 150,
+            "new_listings": 12
+        }
+    })
